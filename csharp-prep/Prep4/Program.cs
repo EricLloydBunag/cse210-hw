ab70c6eeeb146e6afc60f7d4d0c69872d6ff9ec2
EricLloydBunag <elmbunag14@gmail.com>
2024-04-25 18:51:38 +0800

    Completed Week 01 Prep 4.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numlist = new List<int>();
+        int usernum;
+
+        do{
+        Console.WriteLine("Input a number; 0 ends the program: ");
+        usernum = int.Parse(Console.ReadLine());
+        numlist.Add(usernum);
+        } while(usernum != 0);
+
+        Console.WriteLine("The numbers you have inputted are:");
+        int sum = 0;
+        int maxValue = 0;
+        foreach (int num in numlist){
+            Console.WriteLine(num);
+            sum += num;
+            if (num > maxValue){
+                maxValue = num;
+            }
+        }
+        double average = (double)sum / numlist.Count;
+
+        Console.WriteLine($"Max value is {maxValue}, and the sum of all numbers is {sum} with an average of {average}.");
     }
 }
