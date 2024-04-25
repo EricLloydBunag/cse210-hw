2f46159204055c652c2c00072a2f3ed1924f5a12
EricLloydBunag <elmbunag14@gmail.com>
2024-04-25 20:08:00 +0800

    Completed Week 01 Prep 5.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,37 @@ using System;
 
 class Program
 {
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your name? ");
+        string username = Console.ReadLine();
+        return username;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int square= number * number;
+        return square;
+    }
+    static void DisplayResult(string username, int square)
+    {
+        Console.WriteLine($"{username}, your favorite number squared is {square}!");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        string username = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(username, square);
     }
 }
