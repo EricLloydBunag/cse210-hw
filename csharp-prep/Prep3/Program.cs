531bae927abc458e0ff07fea6241f83734905537
EricLloydBunag <elmbunag14@gmail.com>
2024-04-25 16:21:32 +0800

    Week 1 Prep 3 has been completed.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randnum = new Random();
+        int number = randnum.Next(1,100);
+        int userguess;
+        do {
+        Console.WriteLine("Guess the number: ");
+        userguess = int.Parse(Console.ReadLine());
+        if (userguess > number)
+        {
+            Console.WriteLine("Your guess is higher.");
+        }
+        else if (userguess < number)
+        {
+            Console.WriteLine("Your guess is lower.");
+        }
+        } while (userguess != number);
+
+        Console.WriteLine($"You got it right! The answer is {number}.");
     }
 }
