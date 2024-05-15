e7f76f0d2880fc8ec7fbeace61f1a0dc740823ce
EricLloydBunag <elmbunag14@gmail.com>
2024-05-15 20:52:31 +0800

    Finished learning activity 4.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment assignment= new MathAssignment("Leucis Faringray", "C#", "6.4", "5-7");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(assignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment= new WritingAssignment("Arwaine Lancaster", "Physical Fitness", "On Good Health");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
