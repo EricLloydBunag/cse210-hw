78f1c2006be1718be33c25d585198ed1a267b6f8
EricLloydBunag <elmbunag14@gmail.com>
2024-05-21 21:24:27 +0800

    Completed learning activity 05.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> list = new List<Shape>(){
+            new Square("red", 4),
+            new Rectangle("blue", 2, 4.5),
+            new Circle("yellow", 3.5)
+        };
+        foreach (Shape shape in list){
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
