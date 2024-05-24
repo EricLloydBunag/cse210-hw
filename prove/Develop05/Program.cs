266e053df317bff8eeac3ccdaacda95bb8437ac6
EricLloydBunag <elmbunag14@gmail.com>
2024-05-24 16:40:50 +0800

    Added comments to Program.cs for creativity requirements.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     public static void Main(string[] args)
     {
+        //Added levels and milestones for creativity.
         GoalManager goalManager = new GoalManager();
         goalManager.Start();
     }
